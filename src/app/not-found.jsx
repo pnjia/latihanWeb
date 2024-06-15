926777e4095b6dae1309d62cc5aea324c02d9178
pnjia <panjiangkasaputra67@gmail.com>
2024-06-15 13:54:16 +0700

    menambahkan pencarian tidak ditemukan, ketika film yang dicari tidak ada

---
+++ src/app/not-found.jsx
@@ -0,0 +1,8 @@
+const Page = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="font-bold">Tidak ditemukan...</h1>
+    </div>
+  );
+};
+export default Page;
--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -12,9 +12,13 @@ const Page = async ({ params }) => {
   };
   const searchApi = await search(keyword);
   console.log({ api: searchApi });
-  return (
+  return searchApi && searchApi.length > 0 ? (
     <div>
       <Movie api={searchApi} title={title} apiImage={image} />
+    </div>
+  ) : (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-xl font-bold">Pencarian untuk {keyword} tidak ditemukan...</h1>
     </div>
   );
 };
